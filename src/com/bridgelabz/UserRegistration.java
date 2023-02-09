165093c3e4d590a8ddc402f27964132b0005efe8
vijikumar123 <vk9164940756@gmail.com>
2023-02-10 00:19:53 +0530

    Added: uc3 Contact Number Validation

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -24,13 +24,38 @@ public class UserRegistration {
             System.out.println("invalid");
         }
     }
+    //UC-3
+    public void  validEmail(){
+
+        Pattern pattern = Pattern.compile("^(.+)@(.+)$");
+        Matcher matcher = pattern.matcher("bhartif123@gmail.com");
+        if (matcher.matches()){
+            System.out.println("true");
+        }else {
+            System.out.println("false");
+        }
+    }
+    //Uc-4
+    public  void validContactNumber(){
+
+        Pattern pattern = Pattern.compile("^[0-9]{1,2}\\s{1}[0-9]{10}$");
+        Matcher matcher = pattern.matcher("91 9146075923");
+        if (matcher.matches()){
+            System.out.println("valid");
+        }else {
+            System.out.println("invalid");
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Welcome To Regular Expression");
         UserRegistration regex = new UserRegistration();
         regex.validFirstName();
         regex.validLastName();
+        regex.validEmail();
+        regex.validContactNumber();
 
     }
 }
+
 
 
