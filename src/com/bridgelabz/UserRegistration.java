7c6386b9c09f43b9713bbc69880505bfcc5b6e92
vijikumar123 <vk9164940756@gmail.com>
2023-02-10 00:29:26 +0530

    Added: uc7-PassWord3-Valid

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -35,15 +35,61 @@ public class UserRegistration {
             System.out.println("false");
         }
     }
+    //Uc-4
+    public  void validContactNumber(){
+
+        Pattern pattern = Pattern.compile("^[0-9]{1,2}\\s{1}[0-9]{10}$");
+        Matcher matcher = pattern.matcher("91 9146075923");
+        if (matcher.matches()){
+            System.out.println("valid");
+        }else {
+            System.out.println("invalid");
+        }
+    }
+    // UC-5
+    public void validPasswordRule1(){
+        Pattern pattern = Pattern.compile("^[a-z]{8,}+$");
+        Matcher matcher = pattern.matcher("bhartiff");
+        if (matcher.matches()){
+            System.out.println("valid");
+        }else {
+            System.out.println("invalid");
+        }
+    }
+    // UC-6
+    public void validPasswordRule2() {
+        Pattern pattern = Pattern.compile("^([A-Z]?[a-z]{8,})$");
+        Matcher matcher = pattern.matcher("Bhartifule");
+        if (matcher.matches()) {
+            System.out.println("valid");
+        } else {
+            System.out.println("invalid");
+        }
+    }
+    //UC-7
+    public void validPasswordRule3() {
+        Pattern pattern = Pattern.compile("^([A-Z]?[a-z](?=.*[0-9]).{8,})$");
+        Matcher matcher = pattern.matcher("Bharti1222");
+        if (matcher.matches()) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }}
+
     public static void main(String[] args) {
         System.out.println("Welcome To Regular Expression");
         UserRegistration regex = new UserRegistration();
         regex.validFirstName();
         regex.validLastName();
         regex.validEmail();
+        regex.validContactNumber();
+        regex.validPasswordRule1();
+        regex.validPasswordRule2();
+        regex.validPasswordRule3();
 
     }
 }
+
 
 
 
