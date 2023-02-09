cbd9729c1d5dd11cf46e6b2cbf49de3ca57c8209
vijikumar123 <vk9164940756@gmail.com>
2023-02-10 00:41:03 +0530

    Added: uc5 Password validation

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -1,6 +1,5 @@
 package com.bridgelabz;
 
-import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -50,57 +49,11 @@ public class UserRegistration {
     // UC-5
     public void validPasswordRule1(){
         Pattern pattern = Pattern.compile("^[a-z]{8,}+$");
-        Matcher matcher = pattern.matcher("bhartiff");
+        Matcher matcher = pattern.matcher("bhartifule");
         if (matcher.matches()){
             System.out.println("valid");
         }else {
             System.out.println("invalid");
-        }
-    }
-    // UC-6
-    public void validPasswordRule2() {
-        Pattern pattern = Pattern.compile("^([A-Z]?[a-z]{8,})$");
-        Matcher matcher = pattern.matcher("Bhartifule");
-        if (matcher.matches()) {
-            System.out.println("valid");
-        } else {
-            System.out.println("invalid");
-        }
-    }
-    //UC-7
-    public void validPasswordRule3() {
-        Pattern pattern = Pattern.compile("^([A-Z]?[a-z](?=.*[0-9]).{8,})$");
-        Matcher matcher = pattern.matcher("Bharti1222");
-        if (matcher.matches()) {
-            System.out.println("true");
-        } else {
-            System.out.println("false");
-        }}
-    //Uc-8
-    public void validPasswordRule4() {
-        Pattern pattern = Pattern.compile("^([A-Z]?[a-z](?=.*[0-9]).{8,})$");
-        Matcher matcher = pattern.matcher("Arati1992");
-        if (matcher.matches()) {
-            System.out.println("true");
-        } else {
-            System.out.println("false");
-        }}
-    public  void emailAllPattern(){
-        ArrayList<String> emails = new ArrayList<String>();
-        emails.add("abc-100@yahoo.com");
-        emails.add(" abc@yahoo.com,");
-        emails.add("abc.100@yahoo.com");
-        emails.add("abc111@abc.com");
-        emails.add("abc.100@abc.com.au");
-        emails.add("abc@1.com,");
-        //invalid emails
-        emails.add(".abc123#gmail.a");
-        emails.add("abc@*@gmail.com ");
-        String regex = "^(.+)@(.+)$";
-        Pattern pattern = Pattern.compile(regex);
-        for(String email : emails){
-            Matcher matcher = pattern.matcher(email);
-            System.out.println(email +" : "+ matcher.matches()+"\n");
         }
     }
 
@@ -112,13 +65,10 @@ public class UserRegistration {
         regex.validEmail();
         regex.validContactNumber();
         regex.validPasswordRule1();
-        regex.validPasswordRule2();
-        regex.validPasswordRule3();
-        regex.validPasswordRule4();
-        regex.emailAllPattern();
 
     }
 }
+
 
 
 
